614b01c2b7072f34af2d0cd734d7865eb7606c77
AeroztaR <jorguinhos89@gmail.com>
2020-08-22 06:33:57 +0500

    Add fetch method, add jquery method

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     let url = 'https://reqres.in/api/users',
-        xhrbtn = document.querySelector('.xhr'),
-        fetchbtn = document.querySelector('.fetch'),
-        jqbtn = document.querySelector('.jq'),
-        axiosbtn = document.querySelector('.axios'),
-        result = document.querySelector('.users');
+        xhrBtn = document.querySelector('.xhr'),
+        fetchBtn = document.querySelector('.fetch'),
+        jqBtn = document.querySelector('.jq'),
+        axiosBtn = document.querySelector('.axios');
+
+    function createCards(response) {
+        response.data.forEach(item => {
+            let card = document.createElement('div');
+
+            card.classList.add('card');
+
+            card.innerHTML = `
+                <p>${item.first_name} ${item.last_name}</p>
+                <p>${item.email}</p>
+                <img src="${item.avatar}">
+            `;
+
+            document.querySelector('.wrapper').appendChild(card);
+        });
+    }    
 
     // xhr 
-    xhrbtn.addEventListener('click', () => {
+    xhrBtn.addEventListener('click', () => {
         const request = new XMLHttpRequest();
 
         request.open('GET', 'https://reqres.in/api/users');
@@ -17,29 +32,36 @@ window.addEventListener('DOMContentLoaded', () => {
         request.addEventListener('load', () => {
             if (request.readyState === 4 && request.status === 200) {
                 let usersData = JSON.parse(request.response);
-                
-                usersData.data.forEach(item => {
-                    let card = document.createElement('div');
-
-                    card.classList.add('card');
-
-                    card.innerHTML = `
-                        <p>${item.first_name} ${item.last_name}</p>
-                        <p>${item.email}</p>
-                        <img src="${item.avatar}">
-                    `;
-
-                    document.querySelector('.wrapper').appendChild(card);
-                });
+                createCards(usersData); 
             } else {
-                let error = document.createElement('p');
-                error.innerHTML = 'Error!!!';
-                document.querySelector('.wrapper').appendChild(error);
+                alert('Error!!!');
             }
         });
-    }, {'once': true})
+    }, {'once': true});
 
-    
+    // fetch
+    fetchBtn.addEventListener('click', () => {
+        fetch(url)
+        .then((res) => {
+            res.json().then((usersData) => {
+                createCards(usersData);
+            })
+        })
+        .catch(() => {
+            alert('Error!!!');
+        })
+    }, {'once': true});
+
+    // jquery
+    $('.jq').one('click', function(){
+        $.getJSON(url)
+        .done(function(usersData){
+            createCards(usersData);
+        })
+        .fail(function(){
+            alert('Error!!!');
+        })
+    });
 
 
 
@@ -51,8 +73,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
+ 
 
 
 
